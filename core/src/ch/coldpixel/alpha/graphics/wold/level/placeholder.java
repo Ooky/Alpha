75de7aee72f75b708c428cf7ad41a74aaa628e6b
mike amacker <mike.amacker@gmx.ch>
2014-12-23 17:31:29 +0100

    Create package structure. Added placeholder fileds

---
+++ core/src/ch/coldpixel/alpha/graphics/wold/level/placeholder.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.coldpixel.alpha.graphics.wold.level;
+
+/**
+ *
+ * @author Mike
+ */
+public class placeholder {
+    
+}
