937be84321def4b212221cd6cbd7954cbba6c416
Luca Dias <luca.dias@hotmail.ch>
2015-01-11 14:45:27 +0100

    First Menu Try
    You can add new Buttons. But the actionlistener dont work yet

--- core/src/ch/coldpixel/alpha/main/MainMenu.java
+++ core/src/ch/coldpixel/alpha/main/MainMenu.java
@@ -7,11 +7,16 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 /**
  *
@@ -39,29 +44,57 @@ public class MainMenu extends ApplicationAdapter implements Screen {
     Boolean showFPS;
     //Level
     Level level;
-    //MainMenu
-    private MainMenu menu;
+
     //TextButton
     TextButton buttonPlay;
+    TextButton editorButton;
     //Skin
     private Skin skin;
     //Stage
     private Stage stage;
-    //Table
-    private Table table;
-
 
 //==============================================================================
 //Methods
 //==============================================================================
     @Override
     public void create() {
-        skin = new Skin(Gdx.files.internal("Graphics/MenuSkins/uiskin.json"),
-                new TextureAtlas(Gdx.files.internal("Graphics/MenuSkins/uiskin.atlas")));
-
-        buttonPlay = new TextButton("Hi", skin);
         stage = new Stage();
-        table = new Table();
+        Gdx.input.setInputProcessor(stage);// Make the stage consume events
+
+        createBasicSkin();
+
+        TextButton editorButton = new TextButton("Editor", skin); // Use the initialized skin
+        editorButton.setPosition(Gdx.graphics.getWidth() / 2 - Gdx.graphics.getWidth() / 8, Gdx.graphics.getHeight() / 2);
+        stage.addActor(editorButton);
+
+        TextButton newGameButton = new TextButton("New game", skin); // Use the initialized skin
+        newGameButton.setPosition(Gdx.graphics.getWidth() / 2 - Gdx.graphics.getWidth() / 8, Gdx.graphics.getHeight() / 4);
+        stage.addActor(newGameButton);
+
+//        show();
+
+    }
+
+    private void createBasicSkin() {
+        //Create a font
+        BitmapFont font = new BitmapFont();
+        skin = new Skin();
+        skin.add("default", font);
+
+        //Create a texture
+        Pixmap pixmap = new Pixmap((int) Gdx.graphics.getWidth() / 4, (int) Gdx.graphics.getHeight() / 10, Pixmap.Format.RGB888);
+        pixmap.setColor(Color.WHITE);
+        pixmap.fill();
+        skin.add("background", new Texture(pixmap));
+
+        //Create a button style
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("background", Color.GRAY);
+        textButtonStyle.down = skin.newDrawable("background", Color.DARK_GRAY);
+        textButtonStyle.checked = skin.newDrawable("background", Color.DARK_GRAY);
+        textButtonStyle.over = skin.newDrawable("background", Color.LIGHT_GRAY);
+        textButtonStyle.font = skin.getFont("default");
+        skin.add("default", textButtonStyle);
 
     }
 
@@ -83,6 +116,13 @@ public class MainMenu extends ApplicationAdapter implements Screen {
 
     @Override
     public void show() {
+
+        Gdx.input.setInputProcessor(stage);
+        editorButton.addListener(new ClickListener() {
+            public void clicked(InputEvent event) {
+                System.out.println("hi");
+            }
+        });
 
     }
 
